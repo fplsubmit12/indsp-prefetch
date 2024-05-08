67587fd99eb9cc75121c3a601ed2db78cfd94848
fplsubmit12 <169025897+fplsubmit12@users.noreply.github.com>
2024-05-08 10:55:50 +0800

    Add files via upload
    update

---
+++ dsp_chain_has_en_direct_inst.v
@@ -0,0 +1,490 @@
+`timescale 1ns/1ps
+
+module dsp_chain_has_en_direct_inst (
+  input      [15:0]   a,
+  input      [15:0]   b_0,
+  input      [15:0]   b_1,
+  input      [15:0]   b_2,
+  input      [15:0]   b_3,
+  output     [31:0]   p_out,
+  input               ena_0,
+  input               ena_1,
+  input               ena_2,
+  input               ena_3,
+  input               ena_d_0,
+  input               ena_d_1,
+  input               ena_d_2,
+  input               ena_d_3,
+  input               clk
+);
+
+  reg        [4:0]    dsp48e2s_0_INMODE;
+  reg        [8:0]    dsp48e2s_0_OPMODE;
+  wire       [29:0]   dsp48e2s_0_A;
+  wire       [17:0]   dsp48e2s_0_B;
+  reg        [4:0]    dsp48e2s_1_INMODE;
+  reg        [8:0]    dsp48e2s_1_OPMODE;
+  wire       [29:0]   dsp48e2s_1_A;
+  reg        [4:0]    dsp48e2s_2_INMODE;
+  reg        [8:0]    dsp48e2s_2_OPMODE;
+  wire       [29:0]   dsp48e2s_2_A;
+  reg        [4:0]    dsp48e2s_3_INMODE;
+  reg        [8:0]    dsp48e2s_3_OPMODE;
+  wire       [29:0]   dsp48e2s_3_A;
+  wire       [29:0]   dsp48e2s_0_ACOUT;
+  wire       [17:0]   dsp48e2s_0_BCOUT;
+  wire       [47:0]   dsp48e2s_0_PCOUT;
+  wire       [0:0]    dsp48e2s_0_CARRYCASCOUT;
+  wire       [0:0]    dsp48e2s_0_MULTSIGNOUT;
+  wire       [47:0]   dsp48e2s_0_P;
+  wire       [3:0]    dsp48e2s_0_CARRYOUT;
+  wire       [7:0]    dsp48e2s_0_XOROUT;
+  wire                dsp48e2s_0_OVERFLOW;
+  wire                dsp48e2s_0_UNDERFLOW;
+  wire                dsp48e2s_0_PATTERNBDETECT;
+  wire                dsp48e2s_0_PATTERNDETECT;
+  wire       [29:0]   dsp48e2s_1_ACOUT;
+  wire       [17:0]   dsp48e2s_1_BCOUT;
+  wire       [47:0]   dsp48e2s_1_PCOUT;
+  wire       [0:0]    dsp48e2s_1_CARRYCASCOUT;
+  wire       [0:0]    dsp48e2s_1_MULTSIGNOUT;
+  wire       [47:0]   dsp48e2s_1_P;
+  wire       [3:0]    dsp48e2s_1_CARRYOUT;
+  wire       [7:0]    dsp48e2s_1_XOROUT;
+  wire                dsp48e2s_1_OVERFLOW;
+  wire                dsp48e2s_1_UNDERFLOW;
+  wire                dsp48e2s_1_PATTERNBDETECT;
+  wire                dsp48e2s_1_PATTERNDETECT;
+  wire       [29:0]   dsp48e2s_2_ACOUT;
+  wire       [17:0]   dsp48e2s_2_BCOUT;
+  wire       [47:0]   dsp48e2s_2_PCOUT;
+  wire       [0:0]    dsp48e2s_2_CARRYCASCOUT;
+  wire       [0:0]    dsp48e2s_2_MULTSIGNOUT;
+  wire       [47:0]   dsp48e2s_2_P;
+  wire       [3:0]    dsp48e2s_2_CARRYOUT;
+  wire       [7:0]    dsp48e2s_2_XOROUT;
+  wire                dsp48e2s_2_OVERFLOW;
+  wire                dsp48e2s_2_UNDERFLOW;
+  wire                dsp48e2s_2_PATTERNBDETECT;
+  wire                dsp48e2s_2_PATTERNDETECT;
+  wire       [29:0]   dsp48e2s_3_ACOUT;
+  wire       [17:0]   dsp48e2s_3_BCOUT;
+  wire       [47:0]   dsp48e2s_3_PCOUT;
+  wire       [0:0]    dsp48e2s_3_CARRYCASCOUT;
+  wire       [0:0]    dsp48e2s_3_MULTSIGNOUT;
+  wire       [47:0]   dsp48e2s_3_P;
+  wire       [3:0]    dsp48e2s_3_CARRYOUT;
+  wire       [7:0]    dsp48e2s_3_XOROUT;
+  wire                dsp48e2s_3_OVERFLOW;
+  wire                dsp48e2s_3_UNDERFLOW;
+  wire                dsp48e2s_3_PATTERNBDETECT;
+  wire                dsp48e2s_3_PATTERNDETECT;
+  wire       [29:0]   t_A;
+  wire       [15:0]   t_A_1;
+  wire       [17:0]   t_B;
+  wire       [15:0]   t_B_1;
+  wire       [29:0]   t_A_2;
+  wire       [15:0]   t_A_3;
+  wire       [29:0]   t_A_4;
+  wire       [15:0]   t_A_5;
+  wire       [29:0]   t_A_6;
+  wire       [15:0]   t_A_7;
+  wire                t_OPMODE;
+  wire                t_OPMODE_1;
+  wire                t_OPMODE_2;
+  wire                t_OPMODE_3;
+  function [4:0] zz_dsp48e2s_0_INMODE(input dummy);
+    begin
+      zz_dsp48e2s_0_INMODE[1] = 1'b0;
+      zz_dsp48e2s_0_INMODE[2] = 1'b1;
+      zz_dsp48e2s_0_INMODE[3] = 1'b0;
+      zz_dsp48e2s_0_INMODE[0] = 1'b0;
+      zz_dsp48e2s_0_INMODE[4] = 1'b0;
+    end
+  endfunction
+  wire [4:0] t_1;
+  function [4:0] zz_dsp48e2s_1_INMODE(input dummy);
+    begin
+      zz_dsp48e2s_1_INMODE[1] = 1'b0;
+      zz_dsp48e2s_1_INMODE[2] = 1'b1;
+      zz_dsp48e2s_1_INMODE[3] = 1'b0;
+      zz_dsp48e2s_1_INMODE[0] = 1'b0;
+      zz_dsp48e2s_1_INMODE[4] = 1'b0;
+    end
+  endfunction
+  wire [4:0] t_2;
+  function [4:0] zz_dsp48e2s_2_INMODE(input dummy);
+    begin
+      zz_dsp48e2s_2_INMODE[1] = 1'b0;
+      zz_dsp48e2s_2_INMODE[2] = 1'b1;
+      zz_dsp48e2s_2_INMODE[3] = 1'b0;
+      zz_dsp48e2s_2_INMODE[0] = 1'b0;
+      zz_dsp48e2s_2_INMODE[4] = 1'b0;
+    end
+  endfunction
+  wire [4:0] t_3;
+  function [4:0] zz_dsp48e2s_3_INMODE(input dummy);
+    begin
+      zz_dsp48e2s_3_INMODE[1] = 1'b0;
+      zz_dsp48e2s_3_INMODE[2] = 1'b1;
+      zz_dsp48e2s_3_INMODE[3] = 1'b0;
+      zz_dsp48e2s_3_INMODE[0] = 1'b0;
+      zz_dsp48e2s_3_INMODE[4] = 1'b0;
+    end
+  endfunction
+  wire [4:0] t_4;
+
+  assign t_A_1 = b_0;
+  assign t_A = {{14{t_A_1[15]}}, t_A_1};
+  assign t_B_1 = a;
+  assign t_B = {{2{t_B_1[15]}}, t_B_1};
+  assign t_A_3 = b_1;
+  assign t_A_2 = {{14{t_A_3[15]}}, t_A_3};
+  assign t_A_5 = b_2;
+  assign t_A_4 = {{14{t_A_5[15]}}, t_A_5};
+  assign t_A_7 = b_3;
+  assign t_A_6 = {{14{t_A_7[15]}}, t_A_7};
+  DSP48E2 #(
+    .A_INPUT("DIRECT"),
+    .B_INPUT("DIRECT"),
+    .AMULTSEL("A"),
+    .BMULTSEL("B"),
+    .PREADDINSEL("A"),
+    .USE_MULT("MULTIPLY"),
+    .USE_SIMD("ONE48"),
+    .AREG(2),
+    .BREG(2),
+    .CREG(1),
+    .DREG(1),
+    .ADREG(1),
+    .MREG(1),
+    .PREG(1),
+    .ACASCREG(1),
+    .BCASCREG(1),
+    .CARRYINREG(1),
+    .CARRYINSELREG(1),
+    .INMODEREG(0),
+    .OPMODEREG(0),
+    .ALUMODEREG(1)
+  ) dsp48e2s_0 (
+    .CLK            (clk                      ), //i
+    .ALUMODE        (4'b0000                  ), //i
+    .INMODE         (dsp48e2s_0_INMODE[4:0]   ), //i
+    .OPMODE         (dsp48e2s_0_OPMODE[8:0]   ), //i
+    .CARRYINSEL     (3'b000                   ), //i
+    .ACIN           (30'h00000000             ), //i
+    .BCIN           (18'h00000                ), //i
+    .PCIN           (48'h000000000000         ), //i
+    .CARRYCASCIN    (1'b0                     ), //i
+    .MULTSIGNIN     (1'b0                     ), //i
+    .ACOUT          (dsp48e2s_0_ACOUT[29:0]   ), //o
+    .BCOUT          (dsp48e2s_0_BCOUT[17:0]   ), //o
+    .PCOUT          (dsp48e2s_0_PCOUT[47:0]   ), //o
+    .CARRYCASCOUT   (dsp48e2s_0_CARRYCASCOUT  ), //o
+    .MULTSIGNOUT    (dsp48e2s_0_MULTSIGNOUT   ), //o
+    .CEA1           (1'b1                     ), //i
+    .CEA2           (1'b1                     ), //i
+    .CEB1           (ena_0                    ), //i
+    .CEB2           (ena_d_0                  ), //i
+    .CEC            (1'b0                     ), //i
+    .CED            (1'b0                     ), //i
+    .CEAD           (1'b0                     ), //i
+    .CEM            (1'b1                     ), //i
+    .CEP            (1'b1                     ), //i
+    .CECARRYIN      (1'b0                     ), //i
+    .CECTRL         (1'b0                     ), //i
+    .CEINMODE       (1'b0                     ), //i
+    .CEALUMODE      (1'b0                     ), //i
+    .RSTA           (1'b0                     ), //i
+    .RSTB           (1'b0                     ), //i
+    .RSTC           (1'b0                     ), //i
+    .RSTD           (1'b0                     ), //i
+    .RSTM           (1'b0                     ), //i
+    .RSTP           (1'b0                     ), //i
+    .RSTALLCARRYIN  (1'b0                     ), //i
+    .RSTCTRL        (1'b0                     ), //i
+    .RSTINMODE      (1'b0                     ), //i
+    .RSTALUMODE     (1'b0                     ), //i
+    .A              (dsp48e2s_0_A[29:0]       ), //i
+    .B              (dsp48e2s_0_B[17:0]       ), //i
+    .C              (48'hffffffffffff         ), //i
+    .D              (27'h7ffffff              ), //i
+    .CARRYIN        (1'b0                     ), //i
+    .P              (dsp48e2s_0_P[47:0]       ), //o
+    .CARRYOUT       (dsp48e2s_0_CARRYOUT[3:0] ), //o
+    .XOROUT         (dsp48e2s_0_XOROUT[7:0]   ), //o
+    .OVERFLOW       (dsp48e2s_0_OVERFLOW      ), //o
+    .UNDERFLOW      (dsp48e2s_0_UNDERFLOW     ), //o
+    .PATTERNBDETECT (dsp48e2s_0_PATTERNBDETECT), //o
+    .PATTERNDETECT  (dsp48e2s_0_PATTERNDETECT )  //o
+  );
+  DSP48E2 #(
+    .A_INPUT("DIRECT"),
+    .B_INPUT("CASCADE"),
+    .AMULTSEL("A"),
+    .BMULTSEL("B"),
+    .PREADDINSEL("A"),
+    .USE_MULT("MULTIPLY"),
+    .USE_SIMD("ONE48"),
+    .AREG(2),
+    .BREG(2),
+    .CREG(1),
+    .DREG(1),
+    .ADREG(1),
+    .MREG(1),
+    .PREG(1),
+    .ACASCREG(1),
+    .BCASCREG(1),
+    .CARRYINREG(1),
+    .CARRYINSELREG(1),
+    .INMODEREG(0),
+    .OPMODEREG(0),
+    .ALUMODEREG(1)
+  ) dsp48e2s_1 (
+    .CLK            (clk                      ), //i
+    .ALUMODE        (4'b0000                  ), //i
+    .INMODE         (dsp48e2s_1_INMODE[4:0]   ), //i
+    .OPMODE         (dsp48e2s_1_OPMODE[8:0]   ), //i
+    .CARRYINSEL     (3'b000                   ), //i
+    .ACIN           (30'h00000000             ), //i
+    .BCIN           (dsp48e2s_0_BCOUT[17:0]   ), //i
+    .PCIN           (dsp48e2s_0_PCOUT[47:0]   ), //i
+    .CARRYCASCIN    (1'b0                     ), //i
+    .MULTSIGNIN     (1'b0                     ), //i
+    .ACOUT          (dsp48e2s_1_ACOUT[29:0]   ), //o
+    .BCOUT          (dsp48e2s_1_BCOUT[17:0]   ), //o
+    .PCOUT          (dsp48e2s_1_PCOUT[47:0]   ), //o
+    .CARRYCASCOUT   (dsp48e2s_1_CARRYCASCOUT  ), //o
+    .MULTSIGNOUT    (dsp48e2s_1_MULTSIGNOUT   ), //o
+    .CEA1           (1'b1                     ), //i
+    .CEA2           (1'b1                     ), //i
+    .CEB1           (ena_1                    ), //i
+    .CEB2           (ena_d_1                  ), //i
+    .CEC            (1'b0                     ), //i
+    .CED            (1'b0                     ), //i
+    .CEAD           (1'b0                     ), //i
+    .CEM            (1'b1                     ), //i
+    .CEP            (1'b1                     ), //i
+    .CECARRYIN      (1'b0                     ), //i
+    .CECTRL         (1'b0                     ), //i
+    .CEINMODE       (1'b0                     ), //i
+    .CEALUMODE      (1'b0                     ), //i
+    .RSTA           (1'b0                     ), //i
+    .RSTB           (1'b0                     ), //i
+    .RSTC           (1'b0                     ), //i
+    .RSTD           (1'b0                     ), //i
+    .RSTM           (1'b0                     ), //i
+    .RSTP           (1'b0                     ), //i
+    .RSTALLCARRYIN  (1'b0                     ), //i
+    .RSTCTRL        (1'b0                     ), //i
+    .RSTINMODE      (1'b0                     ), //i
+    .RSTALUMODE     (1'b0                     ), //i
+    .A              (dsp48e2s_1_A[29:0]       ), //i
+    .B              (18'h00000                ), //i
+    .C              (48'hffffffffffff         ), //i
+    .D              (27'h7ffffff              ), //i
+    .CARRYIN        (1'b0                     ), //i
+    .P              (dsp48e2s_1_P[47:0]       ), //o
+    .CARRYOUT       (dsp48e2s_1_CARRYOUT[3:0] ), //o
+    .XOROUT         (dsp48e2s_1_XOROUT[7:0]   ), //o
+    .OVERFLOW       (dsp48e2s_1_OVERFLOW      ), //o
+    .UNDERFLOW      (dsp48e2s_1_UNDERFLOW     ), //o
+    .PATTERNBDETECT (dsp48e2s_1_PATTERNBDETECT), //o
+    .PATTERNDETECT  (dsp48e2s_1_PATTERNDETECT )  //o
+  );
+  DSP48E2 #(
+    .A_INPUT("DIRECT"),
+    .B_INPUT("CASCADE"),
+    .AMULTSEL("A"),
+    .BMULTSEL("B"),
+    .PREADDINSEL("A"),
+    .USE_MULT("MULTIPLY"),
+    .USE_SIMD("ONE48"),
+    .AREG(2),
+    .BREG(2),
+    .CREG(1),
+    .DREG(1),
+    .ADREG(1),
+    .MREG(1),
+    .PREG(1),
+    .ACASCREG(1),
+    .BCASCREG(1),
+    .CARRYINREG(1),
+    .CARRYINSELREG(1),
+    .INMODEREG(0),
+    .OPMODEREG(0),
+    .ALUMODEREG(1)
+  ) dsp48e2s_2 (
+    .CLK            (clk                      ), //i
+    .ALUMODE        (4'b0000                  ), //i
+    .INMODE         (dsp48e2s_2_INMODE[4:0]   ), //i
+    .OPMODE         (dsp48e2s_2_OPMODE[8:0]   ), //i
+    .CARRYINSEL     (3'b000                   ), //i
+    .ACIN           (30'h00000000             ), //i
+    .BCIN           (dsp48e2s_1_BCOUT[17:0]   ), //i
+    .PCIN           (dsp48e2s_1_PCOUT[47:0]   ), //i
+    .CARRYCASCIN    (1'b0                     ), //i
+    .MULTSIGNIN     (1'b0                     ), //i
+    .ACOUT          (dsp48e2s_2_ACOUT[29:0]   ), //o
+    .BCOUT          (dsp48e2s_2_BCOUT[17:0]   ), //o
+    .PCOUT          (dsp48e2s_2_PCOUT[47:0]   ), //o
+    .CARRYCASCOUT   (dsp48e2s_2_CARRYCASCOUT  ), //o
+    .MULTSIGNOUT    (dsp48e2s_2_MULTSIGNOUT   ), //o
+    .CEA1           (1'b1                     ), //i
+    .CEA2           (1'b1                     ), //i
+    .CEB1           (ena_2                    ), //i
+    .CEB2           (ena_d_2                  ), //i
+    .CEC            (1'b0                     ), //i
+    .CED            (1'b0                     ), //i
+    .CEAD           (1'b0                     ), //i
+    .CEM            (1'b1                     ), //i
+    .CEP            (1'b1                     ), //i
+    .CECARRYIN      (1'b0                     ), //i
+    .CECTRL         (1'b0                     ), //i
+    .CEINMODE       (1'b0                     ), //i
+    .CEALUMODE      (1'b0                     ), //i
+    .RSTA           (1'b0                     ), //i
+    .RSTB           (1'b0                     ), //i
+    .RSTC           (1'b0                     ), //i
+    .RSTD           (1'b0                     ), //i
+    .RSTM           (1'b0                     ), //i
+    .RSTP           (1'b0                     ), //i
+    .RSTALLCARRYIN  (1'b0                     ), //i
+    .RSTCTRL        (1'b0                     ), //i
+    .RSTINMODE      (1'b0                     ), //i
+    .RSTALUMODE     (1'b0                     ), //i
+    .A              (dsp48e2s_2_A[29:0]       ), //i
+    .B              (18'h00000                ), //i
+    .C              (48'hffffffffffff         ), //i
+    .D              (27'h7ffffff              ), //i
+    .CARRYIN        (1'b0                     ), //i
+    .P              (dsp48e2s_2_P[47:0]       ), //o
+    .CARRYOUT       (dsp48e2s_2_CARRYOUT[3:0] ), //o
+    .XOROUT         (dsp48e2s_2_XOROUT[7:0]   ), //o
+    .OVERFLOW       (dsp48e2s_2_OVERFLOW      ), //o
+    .UNDERFLOW      (dsp48e2s_2_UNDERFLOW     ), //o
+    .PATTERNBDETECT (dsp48e2s_2_PATTERNBDETECT), //o
+    .PATTERNDETECT  (dsp48e2s_2_PATTERNDETECT )  //o
+  );
+  DSP48E2 #(
+    .A_INPUT("DIRECT"),
+    .B_INPUT("CASCADE"),
+    .AMULTSEL("A"),
+    .BMULTSEL("B"),
+    .PREADDINSEL("A"),
+    .USE_MULT("MULTIPLY"),
+    .USE_SIMD("ONE48"),
+    .AREG(2),
+    .BREG(2),
+    .CREG(1),
+    .DREG(1),
+    .ADREG(1),
+    .MREG(1),
+    .PREG(1),
+    .ACASCREG(1),
+    .BCASCREG(1),
+    .CARRYINREG(1),
+    .CARRYINSELREG(1),
+    .INMODEREG(0),
+    .OPMODEREG(0),
+    .ALUMODEREG(1)
+  ) dsp48e2s_3 (
+    .CLK            (clk                      ), //i
+    .ALUMODE        (4'b0000                  ), //i
+    .INMODE         (dsp48e2s_3_INMODE[4:0]   ), //i
+    .OPMODE         (dsp48e2s_3_OPMODE[8:0]   ), //i
+    .CARRYINSEL     (3'b000                   ), //i
+    .ACIN           (30'h00000000             ), //i
+    .BCIN           (dsp48e2s_2_BCOUT[17:0]   ), //i
+    .PCIN           (dsp48e2s_2_PCOUT[47:0]   ), //i
+    .CARRYCASCIN    (1'b0                     ), //i
+    .MULTSIGNIN     (1'b0                     ), //i
+    .ACOUT          (dsp48e2s_3_ACOUT[29:0]   ), //o
+    .BCOUT          (dsp48e2s_3_BCOUT[17:0]   ), //o
+    .PCOUT          (dsp48e2s_3_PCOUT[47:0]   ), //o
+    .CARRYCASCOUT   (dsp48e2s_3_CARRYCASCOUT  ), //o
+    .MULTSIGNOUT    (dsp48e2s_3_MULTSIGNOUT   ), //o
+    .CEA1           (1'b1                     ), //i
+    .CEA2           (1'b1                     ), //i
+    .CEB1           (ena_3                    ), //i
+    .CEB2           (ena_d_3                  ), //i
+    .CEC            (1'b0                     ), //i
+    .CED            (1'b0                     ), //i
+    .CEAD           (1'b0                     ), //i
+    .CEM            (1'b1                     ), //i
+    .CEP            (1'b1                     ), //i
+    .CECARRYIN      (1'b0                     ), //i
+    .CECTRL         (1'b0                     ), //i
+    .CEINMODE       (1'b0                     ), //i
+    .CEALUMODE      (1'b0                     ), //i
+    .RSTA           (1'b0                     ), //i
+    .RSTB           (1'b0                     ), //i
+    .RSTC           (1'b0                     ), //i
+    .RSTD           (1'b0                     ), //i
+    .RSTM           (1'b0                     ), //i
+    .RSTP           (1'b0                     ), //i
+    .RSTALLCARRYIN  (1'b0                     ), //i
+    .RSTCTRL        (1'b0                     ), //i
+    .RSTINMODE      (1'b0                     ), //i
+    .RSTALUMODE     (1'b0                     ), //i
+    .A              (dsp48e2s_3_A[29:0]       ), //i
+    .B              (18'h00000                ), //i
+    .C              (48'hffffffffffff         ), //i
+    .D              (27'h7ffffff              ), //i
+    .CARRYIN        (1'b0                     ), //i
+    .P              (dsp48e2s_3_P[47:0]       ), //o
+    .CARRYOUT       (dsp48e2s_3_CARRYOUT[3:0] ), //o
+    .XOROUT         (dsp48e2s_3_XOROUT[7:0]   ), //o
+    .OVERFLOW       (dsp48e2s_3_OVERFLOW      ), //o
+    .UNDERFLOW      (dsp48e2s_3_UNDERFLOW     ), //o
+    .PATTERNBDETECT (dsp48e2s_3_PATTERNBDETECT), //o
+    .PATTERNDETECT  (dsp48e2s_3_PATTERNDETECT )  //o
+  );
+  assign t_1 = zz_dsp48e2s_0_INMODE(1'b0);
+  always @(*) dsp48e2s_0_INMODE = t_1;
+  assign t_OPMODE = 1'b0;
+  always @(*) begin
+    dsp48e2s_0_OPMODE[8 : 7] = {t_OPMODE,t_OPMODE};
+    dsp48e2s_0_OPMODE[1 : 0] = {1'b0,1'b1};
+    dsp48e2s_0_OPMODE[3 : 2] = {1'b0,1'b1};
+    dsp48e2s_0_OPMODE[6 : 4] = {2'b00,1'b0};
+  end
+
+  assign dsp48e2s_0_A = t_A;
+  assign dsp48e2s_0_B = t_B;
+  assign t_2 = zz_dsp48e2s_1_INMODE(1'b0);
+  always @(*) dsp48e2s_1_INMODE = t_2;
+  assign t_OPMODE_1 = 1'b0;
+  always @(*) begin
+    dsp48e2s_1_OPMODE[8 : 7] = {t_OPMODE_1,t_OPMODE_1};
+    dsp48e2s_1_OPMODE[1 : 0] = {1'b0,1'b1};
+    dsp48e2s_1_OPMODE[3 : 2] = {1'b0,1'b1};
+    dsp48e2s_1_OPMODE[6 : 4] = {2'b00,1'b1};
+  end
+
+  assign dsp48e2s_1_A = t_A_2;
+  assign t_3 = zz_dsp48e2s_2_INMODE(1'b0);
+  always @(*) dsp48e2s_2_INMODE = t_3;
+  assign t_OPMODE_2 = 1'b0;
+  always @(*) begin
+    dsp48e2s_2_OPMODE[8 : 7] = {t_OPMODE_2,t_OPMODE_2};
+    dsp48e2s_2_OPMODE[1 : 0] = {1'b0,1'b1};
+    dsp48e2s_2_OPMODE[3 : 2] = {1'b0,1'b1};
+    dsp48e2s_2_OPMODE[6 : 4] = {2'b00,1'b1};
+  end
+
+  assign dsp48e2s_2_A = t_A_4;
+  assign t_4 = zz_dsp48e2s_3_INMODE(1'b0);
+  always @(*) dsp48e2s_3_INMODE = t_4;
+  assign t_OPMODE_3 = 1'b0;
+  always @(*) begin
+    dsp48e2s_3_OPMODE[8 : 7] = {t_OPMODE_3,t_OPMODE_3};
+    dsp48e2s_3_OPMODE[1 : 0] = {1'b0,1'b1};
+    dsp48e2s_3_OPMODE[3 : 2] = {1'b0,1'b1};
+    dsp48e2s_3_OPMODE[6 : 4] = {2'b00,1'b1};
+  end
+
+  assign dsp48e2s_3_A = t_A_6;
+  assign p_out = dsp48e2s_3_P[31:0];
+
+endmodule
---
+++ dsp_chain_no_en.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+
+module dsp_chain_no_en(
+    input clk,
+    input signed [15:0] a,
+    input signed [15:0] b_0,
+    input signed [15:0] b_1,
+    input signed [15:0] b_2,
+    input signed [15:0] b_3,
+    output signed [31:0] p_out
+    );
+
+    reg signed [15:0] a_0;
+    reg signed [15:0] a_1;
+    reg signed [15:0] a_2;
+    reg signed [15:0] a_3;
+
+    reg signed [15:0] a_d_0;
+    reg signed [15:0] a_d_1;
+    reg signed [15:0] a_d_2;
+    reg signed [15:0] a_d_3;
+
+    reg signed [15:0] b_d_0;
+    reg signed [15:0] b_d_1;
+    reg signed [15:0] b_d_2;
+    reg signed [15:0] b_d_3;
+
+    reg signed [31:0] m_d_0;
+    reg signed [31:0] m_d_1;
+    reg signed [31:0] m_d_2;
+    reg signed [31:0] m_d_3;
+
+    reg signed [31:0] p_d_0;
+    reg signed [31:0] p_d_1;
+    reg signed [31:0] p_d_2;
+    reg signed [31:0] p_d_3;
+
+    wire signed [31:0] m_0;
+    wire signed [31:0] m_1;
+    wire signed [31:0] m_2;
+    wire signed [31:0] m_3;
+
+    wire signed [31:0] p_0;
+    wire signed [31:0] p_1;
+    wire signed [31:0] p_2;
+    wire signed [31:0] p_3;
+
+    assign m_0 = a_d_0 * b_d_0;
+    assign m_1 = a_d_1 * b_d_1;
+    assign m_2 = a_d_2 * b_d_2;
+    assign m_3 = a_d_3 * b_d_3;
+
+    assign p_0 = m_d_0;
+    assign p_1 = m_d_1 + p_d_0;
+    assign p_2 = m_d_2 + p_d_1;
+    assign p_3 = m_d_3 + p_d_2;
+
+    assign p_out = p_d_3;
+
+    always@(posedge clk) begin
+        a_0 <= a;
+        a_1 <= a_0;
+        a_2 <= a_1;
+        a_3 <= a_2;
+    end
+
+    always@(posedge clk) begin
+        a_d_0 <= a_0;
+        a_d_1 <= a_1;
+        a_d_2 <= a_2;
+        a_d_3 <= a_3;
+    end
+
+    always@(posedge clk) begin
+        b_d_0 <= b_0;
+        b_d_1 <= b_1;
+        b_d_2 <= b_2;
+        b_d_3 <= b_3;
+    end
+
+    always@(posedge clk) begin
+        m_d_0 <= m_0;
+        m_d_1 <= m_1;
+        m_d_2 <= m_2;
+        m_d_3 <= m_3;
+    end
+
+    always@(posedge clk) begin
+        p_d_0 <= p_0;
+        p_d_1 <= p_1;
+        p_d_2 <= p_2;
+        p_d_3 <= p_3;
+    end
+
+
+
+endmodule
